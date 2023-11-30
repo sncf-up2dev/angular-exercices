024335c9aa1ccb9483b91624fe472ec52e4f8e11
MVeyrandSncf <>
2023-11-30 12:06:39 +0100

    Ajout début exercice vidéo player

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -6,8 +6,26 @@ import { Component } from "@angular/core";
     imports: [CommonModule],
 
     selector: 'app-root',
-    template: `This works !`
+    template: `
+        <div class="border" (click)="playing = !playing">
+            {{ playing }}
+        </div>
+
+        <div class="border" (wheel)="onWheel($event)">
+            {{ value }}
+        </div>
+    `
 })
 export class VideoPlayerComponent {
+
+    playing: boolean = false
+    value: number = 0
+
+    onWheel(event: WheelEvent): void {
+        /* Stoppe le comportement par défaut du WheelEvent (scroll) */
+        event.preventDefault()
+
+        /* Le comportement de onWheel à implémenter ici */
+    }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,10 @@ import { AppModule } from './app/demo-project/app.module';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { VideoPlayerComponent } from './app/video-player/video-player.component';
 
-
+/*
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+*/
 
-/*
 bootstrapApplication(VideoPlayerComponent)
   .catch((err) => console.error(err));
-*/
