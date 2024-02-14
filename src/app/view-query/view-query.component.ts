b176ca654fdb4f9075e70498e35abc3f807c0f8b
MVeyrandSncf <ext.mathieu.veyrand@sncf.fr>
2024-02-14 15:09:32 +0100

    Update view-query.component.ts

--- src/app/view-query/view-query.component.ts
+++ src/app/view-query/view-query.component.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <div>
-      <audio #audio src="../assets/sample.mp3" ></audio> 
-      <button (click)="audio.paused ? audio.play() : audio.pause()">Play / Pause</button> 
+    <div *ngFor="let url of urls">
+      <audio controls #audio [src]="url"></audio> 
+      <button (click)="playPause(audio)">Play / Pause</button> 
       <ng-container *ngIf="!audio.paused">Playing...</ng-container>
     </div>
 
@@ -16,10 +16,29 @@ import { Component } from '@angular/core';
       <button (click)="push()">Ajouter piste</button>
       <button (click)="pop()">Supprimer piste</button>
     </div>
+
+    <app-child />
+
   `,
   styles: ``
 })
 export class ViewQueryComponent {
+
+  @ViewChildren('audio')
+  audioElements?: QueryList<ElementRef<HTMLAudioElement>>
+
+  playPause(audio: HTMLAudioElement) {
+    if (audio.paused) {
+      this.audioElements?.forEach(element => {
+        element.nativeElement.pause()
+        element.nativeElement.currentTime = 0
+      });
+    }
+
+    audio?.paused ?
+      audio?.play() :
+      audio?.pause()
+  }
 
   urls: string[] = [
     "../assets/sample.mp3",
@@ -34,12 +53,6 @@ export class ViewQueryComponent {
 
   pop() {
     this.urls.pop()
-  }
-
-  playPause(media: HTMLMediaElement) {
-    media.paused
-      ? media.play()
-      : media.pause()
   }
 
 }
